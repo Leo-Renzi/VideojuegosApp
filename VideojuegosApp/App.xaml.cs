d98a9d5beb123bca88b4c98ae883677b9f94e217
Leonardo Renzi <leorenzi89@gmail.com>
2025-08-19 12:31:53 -0300

    Agregar archivos de proyecto.

---
+++ VideojuegosApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VideojuegosApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
